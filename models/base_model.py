4743d7eb61cb0d5caf3697c8ca1cd2f4c699614f
boluwa111 <giwasamuel6@gmail.com>
2023-05-21 02:47:13 +0000

    ALX assignment

--- models/base_model.py
+++ models/base_model.py
@@ -1,33 +1,34 @@
 #! /usr/bin/python
 
-from uuid import uuid4
+import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """Base model class."""
+    """Base class for all other models."""
 
-    id: str
-    created_at: datetime
-    updated_at: datetime
+    id: str = None
+    created_at: datetime = None
+    updated_at: datetime = None
 
     def __init__(self):
-        self.id = str(uuid4())
+        """Initialize a new BaseModel instance."""
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__
-        )
+        """Return a string representation of the object."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """Update the updated_at attribute."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns a dictionary representation of the object."""
+        """Return a dictionary representation of the object."""
         dict_ = self.__dict__.copy()
         dict_["__class__"] = self.__class__.__name__
-        dict_["created_at"] = dict_["created_at"].isoformat("%Y-%m-%dT%H:%M:%S.%f")
-        dict_["updated_at"] = dict_["updated_at"].isoformat("%Y-%m-%dT%H:%M:%S.%f")
+        dict_["created_at"] = dict_["created_at"].isoformat()
+        dict_["updated_at"] = dict_["updated_at"].isoformat()
         return dict_
