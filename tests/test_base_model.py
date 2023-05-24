67c7257baa676f004faa4be17940a6e4c0de50ae
boluwa111 <giwasamuel6@gmail.com>
2023-05-24 11:16:01 +0000

    ALX assignment

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,44 +1,31 @@
 #!/usr/bin/python3
 
 import unittest
-from datetime import datetime
-from unittest.mock import patch
-from my_module import BaseModel
+
+from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
-    def setUp(self):
-        self.base_model = BaseModel()
 
-    def test_initialization(self):
-        self.assertIsNotNone(self.base_model.id)
-        self.assertIsInstance(self.base_model.id, str)
-        self.assertIsNotNone(self.base_model.created_at)
-        self.assertIsInstance(self.base_model.created_at, datetime)
-        self.assertIsNotNone(self.base_model.updated_at)
-        self.assertIsInstance(self.base_model.updated_at, datetime)
+    def test_init(self):
+        model = BaseModel()
+        self.assertIsNotNone(model.id)
+        self.assertIsNotNone(model.created_at)
+        self.assertIsNotNone(model.updated_at)
 
-    @patch('my_module.datetime')
-    def test_save_updates_updated_at(self, mock_datetime):
-        test_datetime = datetime(2023, 5, 21, 12, 0, 0)
-        mock_datetime.now.return_value = test_datetime
+    def test_save(self):
+        model = BaseModel()
+        model.save()
+        self.assertNotEqual(model.updated_at, model.created_at)
 
-        self.base_model.save()
-
-        self.assertEqual(self.base_model.updated_at, test_datetime)
-
-    def test_to_dict_returns_dict_representation(self):
-        expected_dict = {
-            '__class__': 'BaseModel',
-            'id': self.base_model.id,
-            'created_at': self.base_model.created_at.isoformat(),
-            'updated_at': self.base_model.updated_at.isoformat()
-        }
-
-        result_dict = self.base_model.to_dict()
-
-        self.assertEqual(result_dict, expected_dict)
+    def test_to_dict(self):
+        model = BaseModel()
+        dict_ = model.to_dict()
+        self.assertIn("__class__", dict_)
+        self.assertIn("id", dict_)
+        self.assertIn("created_at", dict_)
+        self.assertIn("updated_at", dict_)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
