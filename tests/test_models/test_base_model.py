14309a85c3dc04d82cd87e93368e4f1327df15cb
boluwa111 <giwasamuel6@gmail.com>
2023-05-25 21:45:54 +0000

    Alx assignment

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,31 +1,33 @@
-#!/usr/bin/python3
 
 import unittest
+import uuid
+from datetime import datetime
 
-from models.base_model import BaseModel
 
-
-class TestBaseModel(unittest.TestCase):
+class BaseModelTest(unittest.TestCase):
 
     def test_init(self):
+        """Test the `__init__` method."""
         model = BaseModel()
-        self.assertIsNotNone(model.id)
-        self.assertIsNotNone(model.created_at)
-        self.assertIsNotNone(model.updated_at)
+        self.assertEqual(model.id, str(uuid.uuid4()))
+        self.assertEqual(model.created_at, datetime.now())
+        self.assertEqual(model.updated_at, datetime.now())
+
+    def test_str(self):
+        """Test the `__str__` method."""
+        model = BaseModel()
+        self.assertEqual(str(model), "[BaseModel] ({}) {}".format(model.__class__.__name__, model.id, model.__dict__))
 
     def test_save(self):
+        """Test the `save` method."""
         model = BaseModel()
         model.save()
-        self.assertNotEqual(model.updated_at, model.created_at)
+        self.assertNotEqual(model.updated_at, datetime.now())
 
     def test_to_dict(self):
+        """Test the `to_dict` method."""
         model = BaseModel()
-        dict_ = model.to_dict()
-        self.assertIn("__class__", dict_)
-        self.assertIn("id", dict_)
-        self.assertIn("created_at", dict_)
-        self.assertIn("updated_at", dict_)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        dic = model.to_dict()
+        self.assertEqual(dic["__class__"], model.__class__.__name__)
+        self.assertEqual(dic["created_at"], model.created_at.isoformat())
+        self.assertEqual(dic["updated_at"], model.updated_at.isoformat())
