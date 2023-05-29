e61eaf2acefaa5e3751407f5b58fbc9eb9243d83
boluwa111 <giwasamuel6@gmail.com>
2023-05-29 00:51:43 +0000

    ALx assignment

--- models/base_model.py
+++ models/base_model.py
@@ -1,45 +1,35 @@
-#! /bin/usr/python3
-
-from uuid import uuid4
+import uuid
 from datetime import datetime
 
-
 class BaseModel:
-    """Base class for all other models."""
-
-    id: str
-    created_at: datetime
-    updated_at: datetime
-
     def __init__(self, *args, **kwargs):
-        self.name = None
-        self.my_number = None
-        if kwargs is not None:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    self.name = kwargs["name"]
-                    self.my_number = kwargs["my_number"]
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            exclude_attrs = ["__class__"]
+            for attr, value in kwargs.items():
+                if attr not in exclude_attrs:
+                    if attr in ["created_at", "updated_at"]:
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, attr, value)
+            if not hasattr(self, "id"):
+                self.id = str(uuid.uuid4())
+            if not hasattr(self, "created_at"):
+                self.created_at = datetime.now()
+            self.updated_at = self.created_at
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.to_dict()}"
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates the public instance attribute `updated_at` with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Return a dictionary representation of the object."""
-        dic = {}
-        dict_ = self.__dict__.copy()
-
-        dict_["__class__"] = self.__class__.__name__
-        dict_["created_at"] = dict_["created_at"].isoformat()
-
-        dict_["updated_at"] = dict_["updated_at"].isoformat()
-        keys = ["id","created_at","my_number","updated_at","name"]
-        for i in keys:
-            dic[i] = dict_[i]
-        return dic
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
